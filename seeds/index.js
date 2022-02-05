7aeff2d8cd412726c57bf6fd7715440984254f0c
Febin Bellamy <febinbellamy@gmail.com>
2022-02-04 20:57:59 -0500

    Styling

--- seeds/index.js
+++ seeds/index.js
@@ -17,10 +17,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-    });
+      image: 'https://source.unsplash.com/collection/483251',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae quis ex dolorum at illum veritatis sunt magni rerum necessitatibus quam, ea reprehenderit quasi atque optio nesciunt vitae exercitationem praesentium quod. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae quis ex dolorum at illum veritatis sunt magni rerum necessitatibus quam, ea reprehenderit quasi atque optio nesciunt vitae exercitationem praesentium quod.',
+      price
+    })
     await camp.save();
   }
 };
