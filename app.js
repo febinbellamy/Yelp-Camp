fd6a70627226181f25bff884e330b007c2d6a988
Febin Bellamy <febinbellamy@gmail.com>
2022-02-09 10:32:09 -0500

    Joi

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const ExpressError = require("./utilities/ExpressError");
 const Campground = require("./models/campground");
 const methodOverride = require("method-override");
 const res = require("express/lib/response");
+const { campgroundSchema } = require("./schemas.js");
+const { join } = require("path");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
@@ -24,6 +26,16 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -43,9 +55,8 @@ app.get("/campgrounds/new", (req, res) => {
 
 app.post(
   "/campgrounds",
+  validateCampground,
   catchAsync(async (req, res, next) => {
-    if (!req.body.campground)
-      throw new ExpressError("Invalid Campground Data", 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -70,6 +81,7 @@ app.get(
 
 app.put(
   "/campgrounds/:id",
+  validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {
