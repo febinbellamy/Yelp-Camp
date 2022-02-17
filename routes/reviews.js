d71b42165558efccccb18ee8388c52901ca9acd0
Febin Bellamy <febinbellamy@gmail.com>
2022-02-17 16:55:39 -0500

    Refactoring

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,6 +3,7 @@ const router = express.Router({ mergeParams: true });
 const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
 const Review = require("../models/review");
 const Campground = require("../models/campground");
+const reviews = require('../controllers/reviews')
 const ExpressError = require("../utilities/ExpressError");
 const catchAsync = require("../utilities/catchAsync");
 
@@ -10,27 +11,12 @@ router.post(
   "/",
   validateReview,
   isLoggedIn,
-  catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash("success", "Created a new review!");
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
+  catchAsync(reviews.createReview)
 );
 
 router.delete(
   "/:reviewId", isLoggedIn, isReviewAuthor,
-  catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success", "Deleted a new review!");
-    res.redirect(`/campgrounds/${id}`);
-  })
+  catchAsync(reviews.deleteReview)
 );
 
 module.exports = router;
